405e8055b0f57de19e134813dff6323ee5dec196
emanoel <emanoel.silva@dce.ufpb.br>
2017-09-20 15:30:12 -0300

    listaIP

--- exercicio08/Lista02/bibGaleriaArte.py
+++ exercicio08/Lista02/bibGaleriaArte.py
@@ -1,4 +1,4 @@
-def consultaPreço(titulo,lista,listaR):
+"""def consultaPreço(titulo,lista,listaR):
 	x=0
 	for i in lista:
 		if titulo == i:
@@ -26,3 +26,27 @@ def consultaTipo(titulo,lista,listaT):
 		if titulo == i:
 			return listaT[x]
 		x+=1
+"""
+
+def consultaPreço(titulo,lista):
+	x=0
+	for i in lista:
+		if titulo == i[0]:
+			return i[1]
+
+def consultaArtista(titulo,lista):
+	for i in lista:
+		if titulo == i[0]:
+			return i[2]
+
+def consultaQuantObras(nome,lista):
+	cont=0
+	for i in lista:
+		if nome == i[2]:
+			cont+=1
+	return cont
+
+def consultaTipo(titulo,lista):
+        for i in lista:
+                if titulo == i[0]:
+                        return i[3]
