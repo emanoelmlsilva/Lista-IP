cd4657aeb096b33109d54909e3dfd4c9ebf1b75f
emanoel <emanoel.silva@dce.ufpb.br>
2017-09-28 18:01:00 -0300

    listaIP

--- exercicio08/Lista03/Revistas.py
+++ exercicio08/Lista03/Revistas.py
@@ -1,22 +1,18 @@
-def pesquisaEditora(lista,titulo,listaE):
-	x=0
-	for i in lista:
-		if titulo == i:
-			return listaE[x]
-		x+=1
+def pesquisaEditora(lista,titulo):
+	for x,i in enumerate(lista):
+		if titulo == i[0]:
+			return i[1]
 
 def pesquisaQuantVendas(titulo,lista):
 	quantV=0
-	for i in lista:
-		if titulo == i:
+	for x,i in enumerate(lista):
+		if titulo == i[0]:
 			quantV+=1
 	return quantV
 
-def pesquisaValor(titulo,lista,listaC):
-	x=0
-	for i in lista:
-		if titulo == i:
-			return listaC[x]
-		x+=1
-	return 0
+def pesquisaValor(titulo,lista):
+	for x,i in enumerate(lista):
+		if titulo == i[0]:
+			return i[2]
+	return None
 
--- exercicio08/Lista03/programarevista.py
+++ exercicio08/Lista03/programarevista.py
@@ -1,21 +1,15 @@
 import Revistas
 lista=[]
-listaE=[]
-listaC=[]
-t = ' '
 while True:
 	t = str.lower(input('Titulo da revista (fim para encerra lista): '))
 	if t == 'fim':
 		break
 	e = str.lower(input('Nome da editora: '))
 	c = float(input('Digite o custo da revista: '))
-	lista.append(t)
-	listaE.append(e)
-	listaC.append(c)
-
+	lista.append([t,e,c])
 print('================================')
 p = str.lower(input('Informe o titulo da Revista para saber a editora: '))
-pe = Revistas.pesquisaEditora(lista,p,listaE)
+pe = Revistas.pesquisaEditora(lista,p)
 print('Editora da revista {} é {}'.format(p,pe))
 
 print('=================================')
@@ -25,14 +19,14 @@ print('Quantidade de exemplares vendidos {} '.format(cont))
 
 print('==================================')
 pc = str.lower(input('Informe o titulo para saber o custo: '))
-custo = Revistas.pesquisaValor(pc,lista,listaC)
+custo = Revistas.pesquisaValor(pc,lista)
 print('Custo da revista {} é {:.2f}'.format(pc,custo))
 
 print('=================================')
 #quantidade de revista editora globo
 conted=0
-for i in range(len(lista)):
-	if listaE[i] == 'editora globo':
+for x,i in enumerate(lista):
+	if i[1] == 'globo':
 		conted += 1
 print('Quantidade de revistas da editora globo {}'.format(conted))
 
@@ -40,32 +34,35 @@ print('=========================================')
 #titulo mais vendido
 maior=0 #1
 v=[]
-revista = ' '
-for i in lista:
+revista = None
+cont=0
+lQuant=[]
+for x,i in enumerate(lista):
 	vend=0
 	res=0
-	l = lista[:]
-	for x in l:
-		if i == x:
+	l=lista[:]
+	for y,c in enumerate(l):
+		if i[0] == c[0]:
 			vend+=1
 			res+=1
-			lista.remove(x)
-	v.append(res)
+			lista.remove(c)
+	if res >= 2: #contagem das revistas com mais de 100 exemplares
+		v.append(res)
+		lQuant.append(i[2])
+		cont+=1
 	if vend > maior:
 		maior = vend
-		revista = i
+		revista = i[0]
 print('Titulo da revista mais vendida {}'.format(revista))
 
 print('=================================')
 #media das revistas com mais de 100 exemplares vendidos
 soma = 0
-x = 0
-for i in v:
-	if i > 100:
-		soma += i
-		x +=1
+for i in lQuant: #soma das revistas com mais de 100 exemplares
+	soma += i
 if soma > 0:
-	media = soma/x
+	media = soma/cont
 else:
 	media = 0
 print('Media das revistas com mais de 100 exemplares vendidos {}'.format(media))
+
